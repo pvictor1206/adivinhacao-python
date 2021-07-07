3049a9d0a9650889facdc5c8b556ad2a20fb1334
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2021-07-07 20:44:49 -0300

    commit

--- bin/jogo_forca.py
+++ bin/jogo_forca.py
@@ -10,16 +10,22 @@ def jogar():
     letras_acertadas = ["_","_","_","_","_","_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     while not enforcou and not acertou:
 
         chute = str(input("Qual letra?: ")).lower().strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute == letra:
-                letras_acertadas[index] = letra
-            index += 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        
+        enforcou = erros == 6
         print(letras_acertadas)
 
 if __name__ == '__main__':
